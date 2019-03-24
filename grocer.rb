82821487b00eefab488f308458b07054669fcf1c
Justin Wu <justinhwu95@gmail.com>
2019-03-24 18:27:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,7 +43,7 @@ def apply_clearance(cart)
     else
       newcart[a][:price] = b[:price]
     end
-    newcart[a][:clearance]
+    newcart[a][:clearance] = b[:clearance]
 end
 
 def checkout(cart, coupons)

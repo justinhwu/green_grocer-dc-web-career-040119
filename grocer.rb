6a0d749a3b3d979fba42d124122e043385630715
Justin Wu <justinhwu95@gmail.com>
2019-03-24 18:01:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,12 +21,12 @@ def apply_coupons(cart, coupons)
     a.each do |b,c|
       coupons.each do |d|
         d.each do |e,f|
-      if b == d[:item] && e[:count] >= d[:num]
-        c[:count] = c[:count] = d[:num]
+      if b == d[:item] && c[:count] >= e[:num]
+        c[:count] = c[:count] = e[:num]
         if newcart.has_key?("#{b} W/COUPON")
           newcart["#{b} W/COUPON"][:count] +=1
         else
-          newcart["#{b} W/COUPON"] = {:price=> d[:cost], :clearance => c[:clearance], :count=> 1}
+          newcart["#{b} W/COUPON"] = {:price=> e[:cost], :clearance => c[:clearance], :count=> 1}
         end
       end
     end

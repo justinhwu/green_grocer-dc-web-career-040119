f390d9340775bb8b531cc49b361f21b937aeb3fa
Justin Wu <justinhwu95@gmail.com>
2019-03-24 18:14:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,6 +22,7 @@ def apply_coupons(cart:[], coupons:[])
         if a == c[:item] && a[:count] >= c[:num]
           b[:count] = b[:count] - c[:num]
           if newcart.has_key?("#{a} W/COUPON")
+            
 
 end
 
